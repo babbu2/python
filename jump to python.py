e1f2ac0557f74235eace6046fd8aab5e5137c565
babbu2 <babbu2@jejunu.ac.kr>
2022-01-01 09:25:19 +0900

    피이썬 공부

---
+++ jump to python.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import dash
+from dash.dependencies import Input, Output
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+
+# jeongsi = pd.read_csv("/users/kimhyeonsu/desktop/shiny/jeongsi/jeongsidata.csv", encoding="euc-kr")
+# print(jeongsi)
